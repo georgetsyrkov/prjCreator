bcfe60ac51be5e283450b6f4a4f46ba116c06a92
Цырков Георгий Александрович <g@tias.pro>
2022-02-17 20:10:38 +0300

    gtk added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
+﻿using System;
+using Gtk;
+
+namespace PrjCreator
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+
+            Window myWin = new Window("My first GTK# Application! ");
+            myWin.Resize(200,200);
+
+            Label myLabel = new Label();
+            myLabel.Text = "Hello World!!!!";
+
+            myWin.Add(myLabel);
+            myWin.ShowAll();
+
+            myWin.DeleteEvent += DelEv;
+
+            Application.Run();
+        }
+
+        private static void DelEv(object sender, DeleteEventArgs e)
+        {
+            Application.Quit();
+        }
+    }
+}
