37ad281453b7b0cfd9c1ebd3377a78d62bc473d5
Цырков Георгий Александрович <g@tias.pro>
2022-02-17 18:24:11 +0300

    init hello world

---
+++ asd.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PrjCreator
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Привет мир");
+        }
+    }
+}
